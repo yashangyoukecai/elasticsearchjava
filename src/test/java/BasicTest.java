687e1d4242cc8f7c6b23d332ae31ae795a4566f7
jinwu <jinwu@mogujie.com>
2016-01-07 14:54:08 +0800

    Test

---
+++ src/test/java/BasicTest.java
@@ -0,0 +1,23 @@
+import com.estest.esBasic.ElasticSearchHandler;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.junit.Test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by wklmogujie on 16/1/7.
+ */
+public class BasicTest {
+    @Test
+    public void TestDelete() {
+        ElasticSearchHandler esHandler = new ElasticSearchHandler();
+        DeleteResponse response = esHandler.deleteIndexResponseById("blog","article","AVIWM_MvOcGLzJiDqIyK");
+        System.out.println("delete result :" + response.isFound());
+    }
+
+}
